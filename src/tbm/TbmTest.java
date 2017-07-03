a18e6c362276e08d40d0044c1914ec942f99097e
denishik@tbm.ru <denishik@tbm.ru>
2017-07-03 12:50:01 +0300

    some change

---
+++ src/tbm/TbmTest.java
@@ -0,0 +1,18 @@
+package tbm;
+
+/**
+ * Created by denishik on 03.07.2017.
+ */
+public class TbmTest {
+
+    private Long a;
+
+    public Long getA() {
+        return a;
+    }
+
+    public void setA(Long a) {
+        this.a = a;
+    }
+}
+
